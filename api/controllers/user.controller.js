44cdbc379eb04b35e285488518b577e5826e25d0
farid555 <farid555a@gmail.com>
2024-10-26 13:12:40 +0300

    Setup users update profile and configure Cloudinary(avatar)

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -47,7 +47,10 @@ export const updateUser = async (req, res) => {
         ...(avatar && { avatar }),
       },
     });
-    res.status(200).json(updatedUser);
+
+    const { password: userPassword, ...rest } = updatedUser;
+
+    res.status(200).json(rest);
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Failed to update user!" });
--- react-estate-ui/src/routes/profileUpdatePage/profileUpdatePage.jsx
+++ react-estate-ui/src/routes/profileUpdatePage/profileUpdatePage.jsx
@@ -1,13 +1,40 @@
 import "./profileUpdatePage.scss";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
+import apiRequest from "../../lib/apiRequest";
+import { useNavigate } from "react-router-dom";
+import UploadWidget from "../../components/uploadWidget/UploadWidget";
 
 function ProfileUpdatePage() {
+  const [error, setError] = useState("");
   const { currentUser, updateUser } = useContext(AuthContext);
+  const [avatar, setAvatar] = useState(currentUser.avatar);
+
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const { username, email, password} = Object.fromEntries(formData);
+
+    try {
+      const res = await apiRequest.put(`/users/${currentUser.id}`, {
+        username,
+        email,
+        password,
+        avatar,
+      });
+      updateUser(res.data);
+      navigate("/profile");
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+      setError(error.response.data.message);
+    }
+  };
   return (
     <div className="profileUpdatePage">
       <div className="formContainer">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>Update Profile</h1>
           <div className="item">
             <label htmlFor="username">Username</label>
@@ -36,10 +63,22 @@ function ProfileUpdatePage() {
             <input id="password" name="password" type="password" />
           </div>
           <button>Update</button>
+          {error && <span>{error}</span>}
         </form>
       </div>
       <div className="sideContainer">
-        <img src={currentUser.avatar  || "/noavatar.png"} alt="" className="avatar" />
+        <img src={avatar || "/noavatar.png"} alt="" className="avatar" />
+        <UploadWidget
+          uwConfig={{
+            cloudName: "dqg9tmmdo",
+            uploadPreset: "estate",
+            multiple: false,
+            maxImageFileSize: 2000000,
+            folder: "avatars",
+            cropping: false,
+          }}
+          setAvatar={setAvatar}
+        />
       </div>
     </div>
   );
